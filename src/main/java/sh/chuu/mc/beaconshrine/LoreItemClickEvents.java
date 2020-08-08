446bced8ad737a967dffb518d5e8fdba985fa25f
Simon Chuu <simonorj@outlook.com>
2020-08-08 05:54:22 -0400

    Require skylight (except in the Nether) to use the teleportation scroll

--- src/main/java/sh/chuu/mc/beaconshrine/LoreItemClickEvents.java
+++ src/main/java/sh/chuu/mc/beaconshrine/LoreItemClickEvents.java
@@ -1,12 +1,19 @@
 package sh.chuu.mc.beaconshrine;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.HeightMap;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import sh.chuu.mc.beaconshrine.utils.BeaconShireItemUtils;
 
@@ -30,8 +37,23 @@ public class LoreItemClickEvents implements Listener {
             BeaconShireItemUtils.WarpScroll ws = getWarpScrollData(item);
             if (ws == null) return;
 
+            Player p = ev.getPlayer();
+            World w = p.getWorld();
+            Location l = p.getLocation();
+            if (w.getEnvironment() != World.Environment.NETHER && w.getHighestBlockYAt(l, HeightMap.MOTION_BLOCKING) > l.getBlockY()) {
+                p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("You require access to skylight to warp"));
+                return;
+            }
             ev.setCancelled(true);
-            plugin.getShrineManager().getShrine(ws.id).warpPlayer(ev.getPlayer()).thenAccept(warped -> {
+
+            if (action == Action.RIGHT_CLICK_AIR) {
+                if (ev.getHand() == EquipmentSlot.OFF_HAND)
+                    p.swingOffHand();
+                else
+                    p.swingMainHand();
+            }
+
+            plugin.getShrineManager().getShrine(ws.id).warpPlayer(p).thenAccept(warped -> {
                 if (warped) item.setAmount(item.getAmount() - 1);
             });
         }
